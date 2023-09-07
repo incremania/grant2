6b778ccdd217b8027fd978a55e957cf166818835
incremania <106455212+incremania@users.noreply.github.com>
2023-09-07 21:20:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
-const express = require('expres');
+const express = require('express');
 const app = express();
 const path = require('path');
 const session = require('express-session');
